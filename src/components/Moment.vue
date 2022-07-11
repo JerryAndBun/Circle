7ec3b0cc5fa79f1558b6ccb79a81283eee667843
JerryAnDBUn <1216683759@qq.com>
2022-07-11 23:24:11 +0800

    实现动态过长省略，点击打开的功能

--- src/components/Moment.vue
+++ src/components/Moment.vue
@@ -48,7 +48,7 @@
     <div class="spreate"></div>
   </div>
   <!-- 转发别人普通文字动态的动态 -->
-  <div class="forwardMoment">
+  <div class="forwardMoment" id="forwardContent">
     <div class="user">
       <img class="avatar" :src='`${baseurl}${item.userInfo.avatar}`'></img>
       <div class="nickname">{{item.userInfo.nickname}}</div>
@@ -60,10 +60,15 @@
         <img src="" alt="" class="userImg">
         <div class="nickName">&nbsp昵称{{}}</div>
       </div>
-      <div class="forwardInner">
-        489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456
+      <div class="textContainer" ref="textContainer" id="textContainer">
+        <div class="forwardInner" ref="forwardInner">
+          <span class="text" ref="text">6489456456489456456489456459456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456
+          6489456456489456456489456459456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456
+          6489456456489456456489456459456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456489456456
+          </span>
+        </div>
       </div>
-        <div class="openup">展开</div>
+      <div class="clickformore" ref="clickformore" @click="unfold" v-if="clickformore">展开</div>
     </main>
     <div class="bottom">
       <div class="likes" @click="test">
@@ -91,7 +96,9 @@ export default {
     return {
       myavatar:this.avatar,
       baseurl:BASE_URL,
-      list:[]
+      list:[],
+      clickformore: false,
+      isfold: true,
     }
   },
   computed:{
@@ -102,12 +109,83 @@ export default {
       this.$store.commit('info/toast_list',{type:'push'})
       console.log(this.toast_list);
     },
-  },
-  mounted() {
-      if(!this.myavatar){
-          this.myavatar=require('../assets/imgs/头像.jpg')
+    check_click_for_more() {
+      this.$nextTick(() => {
+        if (this.$refs.text.offsetHeight > 84) {
+          // 需要检测的div的折叠时的高度，若大于，则折叠
+          this.$nextTick(() => {
+            this.clickformore = true;
+            console.log('要折叠');
+            // overflow: hidden;
+            //     max-height: 144px;
+            //     word-break: break-all;
+            //     text-overflow: ellipsis;
+            //     display: -webkit-box;
+            //     -webkit-line-clamp: 6;
+            //     -webkit-box-orient: vertical;
+            // this.$refs.forwardInner.style.overflow='hidden'
+            // this.$refs.forwardInner.style.maxHeight='144px'
+            // this.$refs.forwardInner.style.wordBreak='break-all'
+            // this.$refs.forwardInner.style.textOverflow='ellipsis'
+            // this.$refs.forwardInner.style.display='-webkit-box'
+            // this.$refs.forwardInner.style.webkitLineClamp='6'
+            // this.$refs.forwardInner.style.webkitBoxOrient='vertical'
+          });
+        } else {
+          this.$nextTick(() => {
+          // 需要检测的div的折叠时的高度，若小于，则默认高度，无需折叠
+            this.clickformore = false;
+            console.log('不要折叠');
+          });
+        }
+      });
+    },
+    unfold() {
+      // 折叠部分的父div
+      let father = document.getElementById("textContainer");
+      if (this.isfold) {
+        // 此处打开
+        // 这些只是出现省略号的代码，看起来有点多
+        this.$refs.forwardInner.style.overflow='none'
+        this.$refs.forwardInner.style.maxHeight='none'
+        this.$refs.forwardInner.style.wordBreak='none'
+        this.$refs.forwardInner.style.textOverflow='none'
+        this.$refs.forwardInner.style.display='block'
+        this.$refs.forwardInner.style.webkitLineClamp='0'
+        this.$refs.forwardInner.style.webkitBoxOrient='none'
+
+        father.style.maxHeight = `none`;
+        father.style.overflow = `none`;
+        this.isfold = false;
+        this.$refs.clickformore.innerHTML = "收起";
+      } else {
+        // 此处折叠
+        // 这些只是出现省略号的代码，看起来有点多
+        this.$refs.forwardInner.style.overflow='hidden'
+        this.$refs.forwardInner.style.maxHeight='144px'
+        this.$refs.forwardInner.style.wordBreak='break-all'
+        this.$refs.forwardInner.style.textOverflow='ellipsis'
+        this.$refs.forwardInner.style.display='-webkit-box'
+        this.$refs.forwardInner.style.webkitLineClamp='6'
+        this.$refs.forwardInner.style.webkitBoxOrient='vertical'
+
+        father.style.maxHeight= `150px`;
+        father.style.overflow = `hidden`;
+        this.isfold = true;
+        this.$refs.clickformore.innerHTML = "展开";
       }
-      this.$refs.content.innerHTML=this.item.content
+    },
+  },
+
+  mounted() {
+    this.check_click_for_more();
+    if(!this.myavatar){
+        this.myavatar=require('../assets/imgs/头像.jpg')
+    }
+    this.$refs.content.innerHTML=this.item.content
+  },
+  updated() {
+    // this.check_click_for_more();
   },
 };
 
